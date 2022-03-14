a3038a650396a52be1a00498dec065c873716a10
thesanjarik <sanjarbakenbaev@gmail.com>
2022-03-14 20:46:35 +0600

    Create magicians.py
    Списки работа с циклами for

---
+++ magicians.py
@@ -0,0 +1,35 @@
+
+'''
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title(), ",was a great trick!")
+'''
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+#     print(magician.title(), ",was a great trick!")
+#     print("I can't wait to see your next trick,", magician.title(), ".\n")
+'''
+fast_foods = ['shawerma', 'gamburger', 'sandwich']
+for fast_food in fast_foods:
+    print("I so like!", fast_food.title())
+    print("I really so love fastfood")
+'''
+
+animals = ['dog', 'cat', 'cow']
+for animal in animals:
+    print("A", animal, "would make a great pet!")
+print("Any of these animals would make a pet!")
+
+
+
+
+
+
+
+
+
+
+
+
+
