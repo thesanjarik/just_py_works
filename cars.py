83f809a083e31c5e62179bca051b57d38e23752a
thesanjarik <sanjarbakenbaev@gmail.com>
2022-03-14 19:45:06 +0600

    Create cars.py
    списки

---
+++ cars.py
@@ -0,0 +1,42 @@
+# lists
+
+# cars = ['bmw', 'audi', 'subaru', 'toyota']
+# cars.sort(reverse=True)
+# print(cars)
+'''
+cars = ['bmw', 'audi', 'subaru', 'toyota']
+print("Here is the original list: ")
+print(cars)
+print("\nHere is sorted list: ")
+print(sorted(cars))
+print("Here is the original list again: ")
+print(cars)
+'''
+
+
+'''
+cars = ['bmw', 'audi', 'subaru', 'toyota']
+print(cars)
+cars.reverse()
+print(cars)
+'''
+
+cars = ['bmw', 'audi', 'subaru', 'toyota']
+print(len(cars))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
